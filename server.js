fcec81326c84bcfecd6e5e2214924eb03b583ffa
kfcampbell <keeg4n.campbell@gmail.com>
2017-09-30 16:07:50 -0700

    Centered game. Added header. WIP collisions.

--- server.js
+++ server.js
@@ -35,26 +35,68 @@ io.on('connection', function(socket){
         };
     });
 
-    socket.on('movement', function(data){
-        var player = players[socket.id] || {};
-        if(data.left && !(player.x - 15 < 0)){
-            player.x -= 5;
-        }
-        if(data.right && !(player.x + 15 > 800)){
-            player.x += 5;
-        }
-        if(data.up && !(player.y - 15 < 0)){
-            player.y -= 5;
-        }
-        if(data.down && !(player.y + 15 > 600)){
-            player.y += 5;
-        }
+    socket.on('movement', function(movement){
+        checkBoundsAndPerformMovement(players, socket.id, movement);
     });
 
     socket.on('disconnect', reason => {
         delete players[socket.id];
     });
 });
+
+function checkBoundsAndPerformMovement(players, socketId, movement){
+        var player = players[socketId] || {};
+        for(var key in players){
+                if(key != socketId){
+                    // check bounds to determine collision or not.
+                    if(determineCollision(movement, player, players[key])){
+                        // broadcast event here to respawn player
+                        delete players[key];
+                    }
+                }
+            }
+        if(movement.left && !(player.x - 15 < 0)){
+            player.x -= 5;
+        }
+        if(movement.right && !(player.x + 15 > 800)){
+            player.x += 5;
+        }
+        if(movement.up && !(player.y - 15 < 0)){
+            player.y -= 5;
+        }
+        if(movement.down && !(player.y + 15 > 600)){
+            player.y += 5;
+        }
+}
+
+function determineCollision(playerOneMovement, playerOne, playerTwo){
+    
+    if(determineXCollision(playerOneMovement, playerOne, playerTwo) && determineYCollision(playerOneMovement, playerOne, playerTwo)){
+        return true;
+    }
+    return false;
+    
+}
+
+function determineXCollision(playerOneMovement, playerOne, playerTwo){
+    if(playerOne.x - 15 >= playerTwo.x + 15 && playerOne.x - 15 <= playerTwo.x + 15){
+        return true;
+    }
+    if(playerOne.x + 15 >= playerTwo.x - 15 && playerOne.x - 15 <= playerTwo.x - 15){
+        return true;
+    }
+    return false;
+}
+
+function determineYCollision(playerOneMovement, playerOne, playerTwo){
+    if(playerOne.y - 15 >= playerTwo.y + 15 && playerOne.y - 15 <= playerTwo.y + 15){
+        return true;
+    }
+    if(playerOne.y + 15 >= playerTwo.y - 15 && playerOne.y + 15 <= playerTwo.y - 15){
+        return true;
+    }
+    return false;
+}
 
 setInterval(function(){
     io.sockets.emit('state', players);
