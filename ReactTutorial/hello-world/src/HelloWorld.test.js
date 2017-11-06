bcf5cfa7b7740deb5c62397a03c3b535947d7ff1
Keegan Campbell <Keegan.Campbell@alaskaair.com>
2017-11-06 09:39:15 -0800

    HelloWorld testing completed. Testing configured correctly.

--- ReactTutorial/hello-world/src/HelloWorld.test.js
+++ ReactTutorial/hello-world/src/HelloWorld.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Enzyme from 'enzyme';
 import { configure, shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import Adapter from 'enzyme-adapter-react-16';
@@ -8,6 +9,7 @@ import HelloWorld from './HelloWorld';
 describe(HelloWorld, () => {
     // setup
     Enzyme.configure({ adapter: new Adapter() });
+
     const name = 'Abraham Lincoln';
     const mockRemoveGreeting = jest.fn();
     const component = shallow(
@@ -26,6 +28,17 @@ describe(HelloWorld, () => {
     // shallow render content test
     it('contains the supplied name', () => {
         expect(component.text()).toContain(name);
+    });
+
+    // interaction tests
+    it('modifies the greeting when frenchify button is clicked', () => {
+        component.find('button.frenchify').simulate('click');
+        expect(component.text()).toContain('Bonjour');
+    });
+
+    it('calls the passed in removeGreeting function when remove button is clicked', () => {
+        component.find('button.remove').simulate('click');
+        expect(mockRemoveGreeting).toBeCalled();
     });
 
 });
