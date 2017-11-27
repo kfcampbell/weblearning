008a89c75da8aefe9b1b62a472f0cc1fa5ac65e5
Keegan Campbell <kfcampbell@Keegans-MacBook-Pro.local>
2017-11-27 07:41:46 -0800

    Soundcloud tutorial completed through 'let's add a store middleware.'

--- SoundCloudTutorial/soundcloud_client/src/index.js
+++ SoundCloudTutorial/soundcloud_client/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './stores/configureStore';
+import * as actions from './actions';
 import Stream from './components/Stream';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -11,6 +13,9 @@ const tracks = [
         title: 'track 1'
     }
 ];
+
+const store = configureStore;
+store.dispatch(actions.setTracks(tracks));
 
 ReactDOM.render(
     <Stream tracks={tracks} />,
