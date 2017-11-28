74d477091d86d9eeadc8fa323a5809845c50e97c
Keegan Campbell <Keegan.Campbell@alaskaair.com>
2017-11-28 09:17:55 -0800

    Added react-redux 2.x.

--- SoundCloudTutorial/soundcloud_client/src/components/Stream.js
+++ SoundCloudTutorial/soundcloud_client/src/components/Stream.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 function Stream({ tracks = []}) {
     return(
@@ -10,6 +11,13 @@ function Stream({ tracks = []}) {
         }
         </div>
     )
+}
+
+function mapStateToProps(state) {
+    const tracks = state.track;
+    return {
+        tracks
+    }
 }
 
 export default Stream;
--- SoundCloudTutorial/soundcloud_client/src/stores/configureStore.js
+++ SoundCloudTutorial/soundcloud_client/src/stores/configureStore.js
@@ -7,5 +7,15 @@ const logger = createLogger();
 const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
 
 export default function configureStore(initialState) {
-    return createStoreWithMiddleware(rootReducer, initialState);
+    const store = createStoreWithMiddleware(rootReducer, initialState);
+
+    // because react redux 2.x doesn't hot reload reducers automatically.
+    if(module.hot) {
+        // enable webpack hot module replacement for reducers
+        module.hot.accept('../reducers', () => {
+            const nextRootReducer = require('../reducers/index');
+            store.replaceReducer(nextRootReducer);
+        });
+    }
+    return store;
 }
