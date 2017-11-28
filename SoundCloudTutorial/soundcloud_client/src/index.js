17d413c2a60658af13f37a45f7eea42f88ca496a
Keegan Campbell <Keegan.Campbell@alaskaair.com>
2017-11-28 10:00:53 -0800

    Begin pivot to spotify.

--- SoundCloudTutorial/soundcloud_client/src/index.js
+++ SoundCloudTutorial/soundcloud_client/src/index.js
@@ -1,10 +1,14 @@
+import SC from 'soundcloud';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './stores/configureStore';
 import * as actions from './actions';
 import Stream from './components/Stream/index'; // should maybe be presenter????
+import { CLIENT_ID, REDIRECT_URI } from './constants/auth';
 import registerServiceWorker from './registerServiceWorker';
+
+SC.initialize({ client_id: CLIENT_ID, redirect_uri: REDIRECT_URI });
 
 const tracks = [
     {
